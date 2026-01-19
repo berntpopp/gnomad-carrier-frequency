07294c424a91d99c924c6644b97f7b22e4933d24
Bernt Popp <bernt.popp@gmail.com>
2026-01-19 13:42:56 +0100

    feat(05-02): create AppBar component with theme toggle
    - Add v-app-bar with application title
    - Add theme toggle button on right side
    - Use useAppTheme composable for toggle functionality
    - Icon and tooltip reactive to current theme state

---
+++ src/components/AppBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>gnomAD Carrier Frequency Calculator</v-app-bar-title>
+
+    <v-spacer />
+
+    <v-btn
+      icon
+      variant="text"
+      :title="tooltipText"
+      @click="toggleTheme()"
+    >
+      <v-icon>{{ themeIcon }}</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+import { useAppTheme } from '@/composables';
+
+const { toggleTheme, tooltipText, themeIcon } = useAppTheme();
+</script>
