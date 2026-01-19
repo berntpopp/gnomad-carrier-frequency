bfb947be77fd8b7c5225168f40b06a222cde9a33
Bernt Popp <bernt.popp@gmail.com>
2026-01-19 15:14:55 +0100

    feat(06-02): enhance AppBar with logo and settings gear
    - Add gCFCalc logo text in app bar title
    - Add density=compact for 48px height
    - Add elevation=2 for subtle shadow
    - Add settings gear icon with tooltip
    - Emit openSettings event on gear click
    - Add tooltip wrapper to theme toggle button

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,17 +1,38 @@
 <template>
-  <v-app-bar>
-    <v-app-bar-title>gnomAD Carrier Frequency Calculator</v-app-bar-title>
+  <v-app-bar density="compact" :elevation="2">
+    <v-app-bar-title class="text-body-1 font-weight-bold">
+      gCFCalc
+    </v-app-bar-title>
 
     <v-spacer />
 
-    <v-btn
-      icon
-      variant="text"
-      :title="tooltipText"
-      @click="toggleTheme()"
-    >
-      <v-icon>{{ themeIcon }}</v-icon>
-    </v-btn>
+    <v-tooltip text="Toggle theme" location="bottom">
+      <template #activator="{ props }">
+        <v-btn
+          v-bind="props"
+          icon
+          variant="text"
+          :title="tooltipText"
+          @click="toggleTheme()"
+        >
+          <v-icon>{{ themeIcon }}</v-icon>
+        </v-btn>
+      </template>
+    </v-tooltip>
+
+    <v-tooltip text="Settings" location="bottom">
+      <template #activator="{ props }">
+        <v-btn
+          v-bind="props"
+          icon
+          variant="text"
+          title="Settings"
+          @click="emit('openSettings')"
+        >
+          <v-icon>mdi-cog</v-icon>
+        </v-btn>
+      </template>
+    </v-tooltip>
   </v-app-bar>
 </template>
 
@@ -19,4 +40,8 @@
 import { useAppTheme } from '@/composables';
 
 const { toggleTheme, tooltipText, themeIcon } = useAppTheme();
+
+const emit = defineEmits<{
+  openSettings: [];
+}>();
 </script>
